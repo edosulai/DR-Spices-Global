0c54cb9eb1925627106c17fe29c4932e8d4d0ca6
Edo Sulai <ngufeel@gmail.com>
2022-02-24 09:34:21 +0700

    Migrasi View Login To SB-ADMIN-2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,65 @@
 require('./bootstrap');
 
-import Alpine from 'alpinejs';
+const $ = require("startbootstrap-sb-admin-2/vendor/jquery/jquery.js");
 
-window.Alpine = Alpine;
+require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.js')($);
 
-Alpine.start();
+require('startbootstrap-sb-admin-2/js/sb-admin-2.js');
+
+const {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} = require('startbootstrap-sb-admin-2/vendor/chart.js/Chart.js');
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+require('startbootstrap-sb-admin-2/js/demo/chart-area-demo.js');
+require('startbootstrap-sb-admin-2/js/demo/chart-pie-demo.js');
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,60 +1,69 @@
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
+        {{-- <x-slot name="logo">
             <x-jet-authentication-card-logo />
-        </x-slot>
+        </x-slot> --}}
 
-        <div class="card-body">
+        <div class="row justify-content-center">
 
-            <x-jet-validation-errors class="mb-3 rounded-0" />
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
-            @if (session('status'))
-                <div class="alert alert-success mb-3 rounded-0" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
 
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="mb-3">
-                    <x-jet-label value="{{ __('Email') }}" />
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
 
-                    <x-jet-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email"
-                                 name="email" :value="old('email')" required />
-                    <x-jet-input-error for="email"></x-jet-input-error>
-                </div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
 
-                <div class="mb-3">
-                    <x-jet-label value="{{ __('Password') }}" />
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
 
-                    <x-jet-input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password"
-                                 name="password" required autocomplete="current-password" />
-                    <x-jet-input-error for="password"></x-jet-input-error>
-                </div>
+                                    <form method="POST" action="{{ route('login') }}" class="user">
+                                        @csrf
 
-                <div class="mb-3">
-                    <div class="custom-control custom-checkbox">
-                        <x-jet-checkbox id="remember_me" name="remember" />
-                        <label class="custom-control-label" for="remember_me">
-                            {{ __('Remember Me') }}
-                        </label>
+                                        <div class="form-group">
+                                            {{-- <x-jet-label value="{{ __('Email') }}" /> --}}
+                                            <x-jet-input class="{{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" placeholder="Enter Email Address..." aria-describedby="emailHelp" :value="old('email')" required />
+                                            <x-jet-input-error for="email"></x-jet-input-error>
+                                        </div>
+                                        <div class="form-group">
+                                            {{-- <x-jet-label value="{{ __('Password') }}" /> --}}
+                                            <x-jet-input class="{{ $errors->has('password') ? ' is-invalid' : '' }}" type="password" name="password" required autocomplete="current-password" />
+                                            <x-jet-input-error for="password"></x-jet-input-error>
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <x-jet-checkbox id="remember_me" name="remember" />
+                                                <label class="custom-control-label" for="remember_me">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <x-jet-button>
+                                            {{ __('Log in') }}
+                                        </x-jet-button>
+                                    </form>
+                                    <hr>
+                                    @if (Route::has('password.request'))
+                                    <div class="text-center">
+                                        <a class="small" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                                    </div>
+                                    @endif
+                                    @if (Route::has('register'))
+                                    <div class="text-center">
+                                        <a class="small" href="{{ route('register') }}">Create an Account!</a>
+                                    </div>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="mb-0">
-                    <div class="d-flex justify-content-end align-items-baseline">
-                        @if (Route::has('password.request'))
-                            <a class="text-muted me-3" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
-                            </a>
-                        @endif
-
-                        <x-jet-button>
-                            {{ __('Log in') }}
-                        </x-jet-button>
-                    </div>
-                </div>
-            </form>
+            </div>
         </div>
     </x-jet-authentication-card>
 </x-guest-layout>
