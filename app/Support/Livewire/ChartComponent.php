929e9f6f6d0564408549f1517cd6b1c1c4370a9a
Edo Sulai <ngufeel@gmail.com>
2022-05-16 15:38:14 +0700

    Empty Database Success

--- app/Support/Livewire/ChartComponent.php
+++ app/Support/Livewire/ChartComponent.php
@@ -13,52 +13,52 @@ use Livewire\Component;
 abstract class ChartComponent extends Component
 {
 
-  /**
-   * @var string|null
-   */
-  public ?string $chart_id = null;
+    /**
+     * @var string|null
+     */
+    public ?string $chart_id = null;
 
-  /**
-   * @var string|null
-   */
-  public ?string $chart_data_checksum = null;
+    /**
+     * @var string|null
+     */
+    public ?string $chart_data_checksum = null;
 
-  /**
-   * @return string
-   */
-  protected abstract function chartClass(): string;
+    /**
+     * @return string
+     */
+    protected abstract function chartClass(): string;
 
-  /**
-   * @return \App\Support\Livewire\ChartComponentData
-   */
-  protected abstract function chartData(): ChartComponentData;
+    /**
+     * @return \App\Support\Livewire\ChartComponentData
+     */
+    protected abstract function chartData(): ChartComponentData;
 
-  /**
-   * @return string
-   */
-  protected abstract function view(): string;
+    /**
+     * @return string
+     */
+    protected abstract function view(): string;
 
-  /**
-   * @return \Illuminate\View\View
-   */
-  public function render(): View
-  {
-    $chart_data = $this->chartData();
+    /**
+     * @return \Illuminate\View\View
+     */
+    public function render(): View
+    {
+        $chart_data = $this->chartData();
 
-    if (!$this->chart_id) {
-      $chart_class = $this->chartClass();
+        if (!$this->chart_id) {
+            $chart_class = $this->chartClass();
 
-      $chart = new $chart_class($chart_data);
+            $chart = new $chart_class($chart_data);
 
-      $this->chart_id = $chart->id;
-    } elseif ($chart_data->checksum() !== $this->chart_data_checksum) {
-      $this->emit('chartUpdate', $this->chart_id, $chart_data->labels(), $chart_data->datasets());
+            $this->chart_id = $chart->id;
+        } elseif ($chart_data->checksum() !== $this->chart_data_checksum) {
+            $this->emit('chartUpdate', $this->chart_id, $chart_data->labels(), $chart_data->datasets());
+        }
+
+        $this->chart_data_checksum = $chart_data->checksum();
+
+        return view($this->view(), [
+            'chart' => ($chart ?? null)
+        ]);
     }
-
-    $this->chart_data_checksum = $chart_data->checksum();
-
-    return view($this->view(), [
-      'chart' => ($chart ?? null)
-    ]);
-  }
 }
